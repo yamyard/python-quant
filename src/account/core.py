cba220b1ffa5ea796ea6007f49eb383b2f308071
yamyard <yamyard@hotmail.com>
2025-07-29 17:58:56 +0800

    Add Account Class

---
+++ src/account/core.py
@@ -0,0 +1,29 @@
+from typing import Dict
+from pydantic import BaseModel
+
+class Account(BaseModel):
+    user_id: str
+    cash: float
+    shares: Dict[str, int] = {}
+
+    # basic manipulating
+    def to_dict(self):
+        return self.model_dump()
+    @classmethod
+    def from_dict(cls, d):
+        return cls(**d)
+    def to_json(self):
+        return self.model_dump_json()
+
+    # create new account
+    def new_account(user_id: str, initial_cash: float) -> Account:
+        return Account(user_id=user_id, cash=initial_cash)
+    
+    # modify shares
+    def add_shares(self, symbol: str, amount: int):
+        self.shares[symbol] = self.shares.get(symbol, 0) + amount
+    def remove_shares(self, symbol: str, amount: int):
+        if symbol in self.shares:
+            self.shares[symbol] -= amount
+            if self.shares[symbol] <= 0:
+                del self.shares[symbol]
